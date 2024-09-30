0534ae24b2ff0bb780c9ea5dc3d0d837bceb931f
subframe7536 <1667077010@qq.com>
2024-09-30 16:53:39 +0800

    feat: add `checkLocalFontPermission` / `parseFontStyleToWeight` / `fontWeightLabels` / `isMonospace`

--- README.md
+++ README.md
@@ -8,14 +8,22 @@ wrapper of experimental [queryLocalFonts](https://developer.mozilla.org/en-US/do
 
 ```ts
 import {
+  checkLocalFontPermission,
+  fontWeightLabels,
+  isMonospace,
   isSupportQueryLocalFonts,
+  parseFontStyleToWeight,
   queryFontList,
-  queryTargetFontBlob,
+  queryTargetFontBlob
 } from 'local-font'
 
-if (isSupportQueryLocalFonts()) {
-  await queryFontList() // ==> FontData[]
+if (isSupportQueryLocalFonts() && (await checkLocalFontPermission()).state === 'granted') {
+  const data = await queryFontList() // ==> FontData[]
   await queryTargetFontBlob('ComicSansMS') // ==> Blob | null
+
+  const ctx = document.createElement('canvas').getContext('2d')!
+  isMonospace(data[0].family) // `true` or `false`
+  fontWeightLabels[parseFontStyleToWeight(data[0].style)] // Regular
 }
 ```
 
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,41 @@
 export interface FontData {
   /**
-   * the family of the font face
+   * The family of the font face
    */
   readonly family: string
   /**
-   * the full name of the font face
+   * The full name of the font face
    */
   readonly fullName: string
   /**
-   * the PostScript name of the font face
+   * The PostScript name of the font face
    */
   readonly postscriptName: string
   /**
-   * the style of the font face
+   * The style of the font face
    */
   readonly style: string
   /**
-   * get a Promise that fulfills with a Blob containing the raw bytes of the underlying font file
+   * Get a Promise that fulfills with a Blob containing the raw bytes of the underlying font file
    */
   readonly blob: () => Promise<Blob>
 }
 
 /**
- * check if support query local fonts
+ * Check if support query local fonts
  */
-export function isSupportQueryLocalFonts() {
-  return 'queryLocalFonts' in globalThis
+export function isSupportQueryLocalFonts(): boolean {
+  return 'queryLocalFonts' in window
+}
+
+export async function checkLocalFontPermission(): Promise<PermissionStatus> {
+  return navigator.permissions.query(
+    { name: 'local-fonts' as PermissionName } satisfies PermissionDescriptor,
+  )
 }
 
 /**
- * query local font list
+ * Query local font list
  *
  * {@link https://developer.mozilla.org/en-US/docs/Web/API/Window/queryLocalFonts MDN Reference}
  */
@@ -38,7 +44,7 @@ export async function queryFontList(): Promise<FontData[]> {
 }
 
 /**
- * query target local font blob
+ * Query target local font blob
  * @param postscriptName font postscript name
  */
 export async function queryTargetFontBlob(postscriptName: string): Promise<Blob | null> {
@@ -46,4 +52,59 @@ export async function queryTargetFontBlob(postscriptName: string): Promise<Blob 
     postscriptNames: [postscriptName],
   })
   return font ? await font.blob() : null
+}
+
+export function isMonospace(ctx: CanvasRenderingContext2D, fontName: string): boolean {
+  const count = (char: string): number => ctx.measureText(char).width
+  ctx.font = `16px ${fontName}`
+  return count('i') === count('M') && count('|') === count('%')
+}
+
+export function parseFontStyleToWeight(style: string): number {
+  style = style.toLowerCase().replace(/[\s\-_]/g, '')
+
+  let face
+  switch (true) {
+    case style.includes('thin'):
+      face = 100
+      break
+    case style.includes('extralight'):
+      face = 200
+      break
+    case style.includes('light'):
+      face = 300
+      break
+    case style.includes('medium'):
+      face = 500
+      break
+    case style.includes('semibold'):
+      face = 600
+      break
+    case style.includes('extrabold'):
+      face = 800
+      break
+    case style.includes('black'):
+    case style.includes('ultrabold'):
+      face = 900
+      break
+    case style.includes('bold'):
+      face = 700
+      break
+    default:
+      face = 400
+  }
+
+  return face
+}
+
+export const fontWeightLabels: Record<number, string> = {
+  100: 'Thin',
+  200: 'ExtraLight',
+  300: 'Light',
+  400: 'Regular',
+  500: 'Medium',
+  600: 'SemiBold',
+  700: 'Bold',
+  800: 'ExtraBold',
+  900: 'Black',
 }
